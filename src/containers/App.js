228e1f692bce9191b63bda1b6c6bc282cc485d7c
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-03-30 21:35:02 +0100

    Add changes in package.json

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import style from './App.css';
-import Title from './components/Title.js';
-import TodoList from './components/TodoList.js';
+import Title from '../components/Title.js';
+import TodoList from '../components/TodoList.js';
 import uuid from 'uuid';
-import {hot} from 'react-hot-loader';
+import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
     constructor(props) {
@@ -18,11 +18,11 @@ class App extends React.Component {
             id: uuid.v4()
         };
         const data = [...this.state.data, todo];
-        this.setState({data});
+        this.setState({ data });
     }
     removeTodo(id) {
         const remainder = this.state.data.filter(todo => todo.id !== id);
-        this.setState({data: remainder});
+        this.setState({ data: remainder });
     }
     render() {
         return (
